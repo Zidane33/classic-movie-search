451f338024b7ef3efbb59e52dec9b8235e0c0ad5
Zidan <midoz393@gmail.com>
2020-06-29 15:22:12 -0400

    Updates

---
+++ precache-manifest.53467cccacc0bb319d508b506a2c66c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f45685515653a3de85efcc041cffa6",
+    "url": "/classic-movie-search/index.html"
+  },
+  {
+    "revision": "4e8a17789ca0847f1586",
+    "url": "/classic-movie-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "216a7e885a44e0dc4fa1",
+    "url": "/classic-movie-search/static/css/main.773a2e97.chunk.css"
+  },
+  {
+    "revision": "4e8a17789ca0847f1586",
+    "url": "/classic-movie-search/static/js/2.7122abcb.chunk.js"
+  },
+  {
+    "revision": "a86e12c740364fc55313311f5c1a00fd",
+    "url": "/classic-movie-search/static/js/2.7122abcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216a7e885a44e0dc4fa1",
+    "url": "/classic-movie-search/static/js/main.c24b4329.chunk.js"
+  },
+  {
+    "revision": "fde93f03185ced1eb0f3",
+    "url": "/classic-movie-search/static/js/runtime-main.90e047c1.js"
+  }
+]);
