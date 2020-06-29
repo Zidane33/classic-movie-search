cd7336f18c724340a4e107ff9c152d3e854a90ee
Zidan <midoz393@gmail.com>
2020-06-29 15:26:30 -0400

    changed hit display

--- src/components/custom-hits/CustomHits.js
+++ src/components/custom-hits/CustomHits.js
@@ -7,7 +7,7 @@ const Hits = ({ hits }) => (
         {hits.map(hit => (
             <Card key={hit.objectID}>
                 <Card.Body>
-                    <Card.Title>{`${hit.title} -- ${hit.year}`}</Card.Title>
+                    <Card.Title>{`${hit.title} - ${hit.year}`}</Card.Title>
                     <Card.Text>{`${hit.cast[0]} ${hit.cast[1] ? `and ${hit.cast[1]}` : ''}`}</Card.Text>
                 </Card.Body>
             </Card>
